e8b502eb1a14384dbd64030ad00c94397017cfa0
savami <sava.v.miljkovic@gmail.com>
2023-07-01 15:34:14 +0200

    Styling of photos on PhotoJobShow

--- resources/js/Pages/PhotoJobs/PhotoJobShow.vue
+++ resources/js/Pages/PhotoJobs/PhotoJobShow.vue
@@ -2,15 +2,84 @@
  <AnimatedBackgroundLayout>
     <h1 class="text-center my-8 text-3xl text-white font-bold">Results</h1>
 
-    <div class="grid grid-cols-3 gap-4">
-        <div v-for="photo in pexelsResponse.photos" :key="photo.id" class="p-4">
-            <img :src="photo.src.medium" :alt="photo.alt" class="w-full h-auto" />
-            <h2 class="text-center mt-2">{{ photo.photographer }}</h2>
-            <p class="text-center"><a :href="photo.url" target="_blank">View on Pexels</a></p>
-        </div>
+
+<!--        <div v-for="photo in pexelsResponse.photos" :key="photo.id" class="p-4">-->
+<!--            <img :src="photo.src.medium" :alt="photo.alt" class="w-full h-auto" />-->
+<!--            <h2 class="text-center mt-2">{{ photo.photographer }}</h2>-->
+<!--            <p class="text-center"><a :href="photo.url" target="_blank">View on Pexels</a></p>-->
+<!--        </div>-->
+
+     <div class="container">
+         <div class="image-container" v-for="photo in pexelsResponse.photos" :key="photo.id">
+             <a :href="photo.url" target="_blank">
+                 <img :src="photo.src.medium" :alt="photo.alt" class="image" />
+                 <div class="overlay">
+                     <p class="photographer-name">{{ photo.photographer }}</p>
+                     <p class="view-on-pexels">View on Pexels</p>
+                 </div>
+             </a>
+         </div>
     </div>
  </AnimatedBackgroundLayout>
 </template>
+
+<style scoped>
+.container {
+    column-count: 3;
+    column-gap: 2em;
+    margin: 0 auto;
+}
+
+.image-container {
+    position: relative;
+    display: inline-block;
+    width: 100%;
+    margin-bottom: 2em;
+    break-inside: avoid;
+    transition: ease-in-out .3s;
+    border-radius: 5px;
+}
+
+.image-container:hover {
+    transform: scale(1.05); /* adjust as needed for desired hover scale effect */
+}
+
+.image-container a {
+    display: block;
+}
+
+.image-container img {
+    width: 100%;
+    height: auto;
+    display: block;
+    border-radius: 5px;
+}
+
+.overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5); /* adjust as needed for desired dimming effect */
+    opacity: 0;
+    transition: opacity 0.3s ease-in-out;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.overlay p {
+    color: #fff;
+    text-align: center;
+}
+
+.image-container:hover .overlay {
+    opacity: 1;
+}
+
+</style>
 
 <script setup>
 import AnimatedBackgroundLayout from "../../Layouts/AnimatedBackgroundLayout.vue";
@@ -20,6 +89,4 @@ const props = defineProps(
         pexelsResponse: Object,
     }
 )
-
-
 </script>
