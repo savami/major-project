042a2ed0d58d54f62b61866c7f5a4e5d478b6e6a
savami <sava.v.miljkovic@gmail.com>
2023-08-05 03:01:25 +0200

    implemented gpt-3.5-turbno

--- app/Services/GptService.php
+++ app/Services/GptService.php
@@ -20,55 +20,85 @@ class GptService
         $text_tone = $form['text_tone'][0];
         $text_language = $form['text_language'];
 
-        // Formulate the prompt for GPT-3
-        $preset_prompt = "As an expert SEO-copywriter, please write an SEO-optimized text using the following information: \n\n";
+        $preset_prompt = "You are an advanced AI language model acting as an expert SEO-copywriter. Your task is to generate high-quality, SEO-optimized text based on the provided details such as subject, primary and secondary keywords, audience intent, call to action, tone, and language. Ensure the text is tailored to the audience's needs and optimized for search engines, using the keywords in a natural and effective manner.";
 
-        $prompt = "{$preset_prompt} Write a {$word_amount}-word {$audience_intent} text about {$subject}. The primary keyword is: '{$primary_keyword}', and the secondary keywords are: '{$secondary_keywords}'. The audience intent is {$audience_intent} and the text should target the '{$call_to_action}' call to action. Please use a {$text_tone} tone. \n\n";
+        $prompt = "Write a {$word_amount}-word {$audience_intent} text about {$subject}. The primary keyword is: '{$primary_keyword}', and the secondary keywords are: '{$secondary_keywords}'. The audience intent is {$audience_intent} and the text should target the '{$call_to_action}' call to action. Please use a {$text_tone} tone. \n\n";
 
         if (strtolower($text_language) !== "english") {
             $prompt .= "The text should be written in {$text_language}.\n\n";
         }
 
-//        $prompt = "{$preset_prompt} Given the primary keyword: '{$primary_keyword}', secondary keywords: '{$secondary_keywords}', the audience intent is '{$audience_intent}', and the call to action is '{$call_to_action}', please generate a concise, SEO-optimized text about the following subject: '{$subject}'. Make sure to address these frequently asked questions: \n\n";
-//
-//        $prompt .= "\nThe text should be written in a '{$text_tone} tone. It should be engaging according to the audience's intent, and it should be roughly '{$word_amount}' words. Make sure to include the primary keyword at least a few times in a natural way, and the secondary keywords at least once. Make sure to think of a subtle way to introduce the call to action, somewhere near the end of the text.  The text should be written in '{$text_lnaguage}'.";
-//
-//        $prompt .= "The following is an example text for reference: \n\n";
-//        $prompt .= "Imagine the fresh air flowing through your hair as you effortlessly glide through the city on your new [electric scooter]. Not just any [scooter], but the [latest model] of the year 2023, which has been touted as the [best electric scooter] by various critics. With its superior speed control, [longer battery life], and comfortable riding experience, it’s not hard to see why. \n\n";
-//        $prompt .= "Now, we understand that you may have questions like 'How long does the battery last?' or 'Is it easy to carry around?' Rest assured, the battery can last for an impressive 50 km on a single charge and yes, it is lightweight, making it easy to carry around when you need to. \n\n";
-//        $prompt .= "So, are you ready to revolutionize your commuting experience? Sign up for our newsletter to get the latest updates and deals on our [electric scooters]. You wouldn't want to miss it!";
-//        $prompt .= "\n\nNote: The words and phrases enclosed in brackets '[ ]' in the example are potential keywords that could be utilized in the text. They serve to attract the attention of search engine algorithms and can significantly enhance the SEO performance of the text. Make sure to follow this type of approach according to the example, but with your own words and phrases based on the given information you've been provided with such as the keywords, word amount, language, text tone, call to action, etc.";
-
         $openAiApiKey = config('openai.api_key');
         $openAiEngine = config('openai.engine');
 
-        // Make the API call
-        $response = Http::withHeaders([
-            'Authorization' => 'Bearer ' . $openAiApiKey,
-            'Content-Type' => 'application/json',
-        ])->post('https://api.openai.com/v1/engines/' . $openAiEngine . '/completions', [
-            'prompt' => $prompt,
-            'max_tokens' => $form['word_amount'] * 10, // 10 tokens per word
-            'temperature' => 0.4, // Not too random, but also not too precise/predictable
-            'top_p' => 1,
-            'n' => 1,
-            'stream' => false,
-            'logprobs' => null,
-            'stop' => ['\n'],
-            'presence_penalty' => 0, // Penalizes words/sentences that are not present in the prompt
-            'frequency_penalty' => 1, // Penalizes words/sentences that are repeated too often
-        ]);
 
-        if ($response->successful()) {
-            $result = $response->json();
-            $seo_text = trim($result['choices'][0]['text']);
+//        Make the API call (for legacy GPT-3 models such as 'text-davinci-003')
+        if ($openAiEngine === 'text-davinci-003') {
+            $response = Http::withHeaders([
+                'Authorization' => 'Bearer ' . $openAiApiKey,
+                'Content-Type' => 'application/json',
+            ])->post('https://api.openai.com/v1/engines/' . $openAiEngine . '/completions', [
+                'prompt' => $prompt,
+                'max_tokens' => $form['word_amount'] * 10, // 10 tokens per word
+                'temperature' => 0.4, // Not too random, but also not too precise/predictable
+                'top_p' => 1,
+                'n' => 1,
+                'stream' => false,
+                'logprobs' => null,
+                'stop' => ['\n'],
+                'presence_penalty' => 0, // Penalizes words/sentences that are not present in the prompt
+                'frequency_penalty' => 1, // Penalizes words/sentences that are repeated too often
+            ]);
 
-            Log::info('Prompt: ' . $prompt . ' | Generated SEO Text: ' . $seo_text);
+            if ($response->successful()) {
+                $result = $response->json();
+                $seo_text = trim($result['choices'][0]['text']);
 
-            return $seo_text;
-        } else {
-            Log::error('OpenAI API request failed: ' . $response->body());
-            return null; // return null if the API request fails
+                Log::info('Prompt: ' . $prompt . ' | Generated SEO Text: ' . $seo_text);
+
+                return $seo_text;
+            } else {
+                Log::error('OpenAI API request failed: ' . $response->body());
+                return null; // return null if the API request fails
+            }
+        }
+
+        if ($openAiEngine === 'gpt-3.5-turbo') {
+            $response = Http::withHeaders([
+                'Authorization' => 'Bearer ' . $openAiApiKey,
+                'Content-Type' => 'application/json',
+            ])->post('https://api.openai.com/v1/engines/gpt-3.5-turbo/completions', [
+                'messages' => [
+                    [
+                        'role' => 'system',
+                        'content' => $preset_prompt
+                    ],
+                    [
+                        'role' => 'user',
+                        'content' => $prompt
+                    ]
+                ],
+                'max_tokens' => $form['word_amount'] * 10, // 10 tokens per word
+                'temperature' => 0.5, // Not too random, but also not too precise/predictable
+                'top_p' => 1,
+                'n' => 1,
+                'stream' => false,
+                'stop' => ['\n'],
+//            'presence_penalty' => 0.1,
+//            'frequency_penalty' => 0.6,
+            ]);
+
+            if ($response->successful()) {
+                $result = $response->json();
+                $seo_text = trim($result['choices'][0]['message']['content']);
+
+                Log::info('Prompt: ' . $prompt . ' | Generated SEO Text: ' . $seo_text);
+
+                return $seo_text;
+            } else {
+                Log::error('OpenAI API request failed: ' . $response->body());
+                return null; // return null if the API request fails
+            }
         }
     }
 
